981a6f577517cd89a455601b85248c692328bf1a
WangQiang <actstrady@gmail.com>
2019-11-27 11:42:14 +0800

    Initial commit

---
+++ src/main/java/com/actstrady/autofilldata/AutofillDataApplication.java
@@ -0,0 +1,13 @@
+package com.actstrady.autofilldata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutofillDataApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AutofillDataApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/actstrady/autofilldata/AutofillDataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.actstrady.autofilldata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutofillDataApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
